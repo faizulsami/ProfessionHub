e618487f544b8daa433f86d809ba3ac31ee57489
faizulsami <faizul.sami2021@gmail.com>
2023-04-11 23:02:18 +0600

    fakedb added

--- src/components/Job_Details/Job_Details.jsx
+++ src/components/Job_Details/Job_Details.jsx
@@ -45,13 +45,13 @@ const Job_Details = () => {
                     <div className='w-96 bg-indigo-100 p-8 pb-4 rounded-lg'>
                         <h2 className='job-title lg:text-2xl pt-3'>Job Details</h2>
                         <hr className='w-80 border-indigo-200 my-4'/>
-                        <p><span className='job-details-custom-2 lg:text-lg'>Salary :</span> <span className='job-details-custom lg:text-lg'>100K - 150K (Per Month)</span></p>
-                        <p><span className='job-details-custom-2 lg:text-lg'>Job Title :</span> <span className='job-details-custom lg:text-lg'>Product Designer</span></p>
+                        <p><span className='job-details-custom-2 lg:text-lg'>Salary :</span> <span className='job-details-custom lg:text-lg'>{matchId.Salary}</span></p>
+                        <p><span className='job-details-custom-2 lg:text-lg'>Job Title :</span> <span className='job-details-custom lg:text-lg'>{matchId.jobTitle}</span></p>
                         <h2 className='job-title lg:text-2xl pt-5'>Contact Information</h2>
                         <hr className='w-80 border-indigo-200 my-4'/>
-                        <p><span className='job-details-custom-2 lg:text-lg'>Phone :</span> <span className='job-details-custom lg:text-lg'>01750-00 00 00</span></p>
-                        <p><span className='job-details-custom-2 lg:text-lg'>Email :</span> <span className='job-details-custom lg:text-lg'>info@gmail.com</span></p>
-                        <p><span className='job-details-custom-2 lg:text-lg'>Address :</span> <span className='job-details-custom lg:text-lg'>Dhanmondi 32, Sukrabad Dhaka, Bangladesh</span></p>
+                        <p><span className='job-details-custom-2 lg:text-lg'>Phone :</span> <span className='job-details-custom lg:text-lg'>{matchId.phone}</span></p>
+                        <p><span className='job-details-custom-2 lg:text-lg'>Email :</span> <span className='job-details-custom lg:text-lg'>{matchId.email}</span></p>
+                        <p><span className='job-details-custom-2 lg:text-lg'>Address :</span> <span className='job-details-custom lg:text-lg'>{matchId.address}</span></p>
                     </div>
                     <Button className='lg:w-96 mt-4 lg:h-14 apply-now-style' gradientDuoTone="purpleToBlue">
                         <p className='lg:text-xl apply-now-style'>Apply Now</p>
--- src/components/PageHeader/PageHeader.jsx
+++ src/components/PageHeader/PageHeader.jsx
@@ -6,7 +6,7 @@ const PageHeader = ({title}) => {
         <div>
             <div className='bg-indigo-200 px-5 pb-40 lg:px-40'>
                 <Header></Header>
-                <h2 className='text-center text-2xl font-bold mt-20 page-title-style text-4xl'>{title}</h2>
+                <h2 className='text-center font-bold mt-20 page-title-style text-4xl'>{title}</h2>
             </div>
         </div>
     );
